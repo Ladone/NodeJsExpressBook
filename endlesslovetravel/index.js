7029c68fed28b4bf5ce1120ef97d04a018cd79f0
Vladislav Pototskiy <echoladone@Vladislavs-MacBook-Pro.local>
2022-02-22 19:41:35 +0200

    some fix

--- endlesslovetravel/index.js
+++ endlesslovetravel/index.js
@@ -4,16 +4,29 @@ const expressHandlebars = require('express-handlebars');
 const app = express();
 const port = process.env.port || 3000;
 
+const fortunes = [
+    'Победи свои страхи, или они победят тебя.',
+    'Рекам нужны истоки.',
+    'Не бойся неведомого.',
+    'Тебя ждет приятный сюрприз.',
+    'Будь проще везде, где только можно',
+];
+
 app.engine('hbs', expressHandlebars.engine({
     defaultLayout: 'main',
     extname: '.hbs',
 }));
 
+app.use(express.static(__dirname + '/public'));
+
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => res.render('home'));
 
-app.get('/about', (req, res) => res.render('about'));
+app.get('/about', (req, res) => {
+    let randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+    res.render('about', {fortune: randomFortune});
+});
 
 app.use((req, res) => {
     res.status(404);
